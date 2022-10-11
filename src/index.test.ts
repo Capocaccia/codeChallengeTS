f293e2bad398834332d8a69e1ba2c975ca145628
ccapocaccia <carter.capocaccia@hilton.com>
2022-10-11 13:42:53 -0500

    initial setup of code challenge repo

--- src/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { start } from "./index";
-
-describe("start", () => {
-  it('Should log "Hello, World!"', () => {
-    const consoleSpy = jest.spyOn(console, "log");
-    start();
-    expect(consoleSpy).toHaveBeenCalledWith("Hello, World!");
-  });
-});
---
+++ src/reverseSequence.ts
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// EXAMPLE:
+// n=5 //returns [5,4,3,2,1]
+export const reverseSeq = (n: number) => {
+  return false;
+};
