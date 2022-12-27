b2ce51cffc7334290ca540f6ac5e35dbfddf57ea
Donray Williams <donrayxwilliams@gmail.com>
2022-12-27 13:16:14 -0600

    Hard | Challenge Complete

--- src/hard/moveZeros.ts
+++ src/hard/moveZeros.ts
@@ -1,11 +1,19 @@
-// DESCRIPTION:
-// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
-
-// EXAMPLE:
-// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
-
 export const moveZeros = (
   arr: (string | number | boolean | null | object | Array<[]>)[]
 ) => {
-  return false;
+  const output: typeof arr = [];
+
+  const nonZeros: typeof arr = [];
+  const zeros: typeof arr = [];
+
+  arr.forEach((value) => {
+    if (value === 0) {
+      zeros.push(value);
+      return;
+    }
+
+    nonZeros.push(value);
+  });
+
+  return output.concat(nonZeros, zeros);
 };
