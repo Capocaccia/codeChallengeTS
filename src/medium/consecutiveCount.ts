8ab996da251688a1558e7b7edb3807fc17ff6600
Donray Williams <donrayxwilliams@gmail.com>
2022-12-27 13:16:04 -0600

    Medium | Challenge Complete

--- src/medium/consecutiveCount.ts
+++ src/medium/consecutiveCount.ts
@@ -1,18 +1,28 @@
-// DESCRIPTION:
-// I want to know the size of the longest consecutive elements of X in Y. You will receive two arguments: items and key. Return the length of the longest segment of consecutive keys in the given items.
-
-// NOTES:
-// The items and the key will be either an integer or a string (consisting of letters only)
-// If the key does not appear in the items, return 0
-
-// EXAMPLE:
-// 90000, 0           //returns integer value of 4
-// "abcdaaadse", "a"  //returns integer value of 3
-// "abcdaaadse", "z"  //returns integer value of 0
-
 export const getConsecutiveItems = (
   items: string | number,
   key: string | number
 ) => {
-  return false;
+  let counter = 1;
+  const longestConsecutiveValues: number[] = [];
+
+  const charactersSplit = Array.from(String(items));
+
+  if (!charactersSplit.includes(String(key))) {
+    return 0;
+  }
+
+  charactersSplit.forEach((value, index) => {
+    if (value === charactersSplit[index + 1]) {
+      counter = counter + 1;
+      return;
+    }
+
+    if (counter > Math.max(...longestConsecutiveValues)) {
+      longestConsecutiveValues.push(counter);
+    }
+
+    counter = 1;
+  });
+
+  return longestConsecutiveValues.pop();
 };
