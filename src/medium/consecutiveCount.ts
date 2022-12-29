f1eaa71a7365a2259d726dad4ca33bc9cd30f818
Joshua Pearson <jpearson@ximasoftware.com>
2022-12-29 13:28:47 -0700

    Consecutive count

--- src/medium/consecutiveCount.ts
+++ src/medium/consecutiveCount.ts
@@ -1,5 +1,7 @@
 // DESCRIPTION:
-// I want to know the size of the longest consecutive elements of X in Y. You will receive two arguments: items and key. Return the length of the longest segment of consecutive keys in the given items.
+// I want to know the size of the longest consecutive elements of X in Y.
+// You will receive two arguments: items and key.
+// Return the length of the longest segment of consecutive keys in the given items.
 
 // NOTES:
 // The items and the key will be either an integer or a string (consisting of letters only)
@@ -14,5 +16,19 @@ export const getConsecutiveItems = (
   items: string | number,
   key: string | number
 ) => {
-  return false;
+  const itemsArray = Array.from(items.toString());
+  const keyString = key.toString();
+  let count = 0;
+  let maxCount = 0;
+  itemsArray.forEach((item) => {
+    if (item === keyString) {
+      count++;
+    } else {
+      if (count > maxCount) {
+        maxCount = count;
+      }
+      count = 0;
+    }
+  });
+  return maxCount > count ? maxCount : count;
 };
