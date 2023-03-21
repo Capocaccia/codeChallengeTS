839ce6819a5bba4bbbc16dd0c530145bcb8a67b8
BenFrancka <benfrancka@gmail.com>
2023-03-21 14:31:29 -0700

    adds moveZeroes function, test is passing

--- src/hard/moveZeros.ts
+++ src/hard/moveZeros.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 // DESCRIPTION:
 // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
@@ -7,5 +8,28 @@
 export const moveZeros = (
   arr: (string | number | boolean | null | object | Array<[]>)[]
 ) => {
-  return false;
+
+  //initialze empty array to eventually be our array with zeros at the end
+  const zerosToTheEnd = [];
+
+  //keep track of number of zeros, which, ironcically, starts as zero.
+  let zeroTally = 0;
+
+  //loop through and check if each element of the array is not equal to zero
+  for (let index = 0; index < arr.length; index++) {
+
+    //if element is not equal to zero, it pushes it to the new array. Once this loop finishes, all elements from original array that are not zeroes should be their original order.
+    if (arr[index] !== 0) {
+      zerosToTheEnd.push(arr[index]);
+    } else {
+      zeroTally++;
+    }
+  }
+
+  //zeroTally should now have the number of zeros from the original array. Loop through each each zero in the original array and push to zerosToTheEnd array.
+  for (let index = 0; index < zeroTally; index++) {
+    zerosToTheEnd.push(0);
+  }
+
+  return zerosToTheEnd;
 };
--- src/medium/consecutiveCount.ts
+++ src/medium/consecutiveCount.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 // DESCRIPTION:
 // I want to know the size of the longest consecutive elements of X in Y. You will receive two arguments: items and key. Return the length of the longest segment of consecutive keys in the given items.
 
