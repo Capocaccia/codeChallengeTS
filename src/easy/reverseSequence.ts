c6c22ee25b4bf827cdeb276deb02ecbb17a5f698
BenFrancka <benfrancka@gmail.com>
2023-03-21 14:11:03 -0700

    adds reverseSequence function, test is passing

--- src/easy/reverseSequence.ts
+++ src/easy/reverseSequence.ts
@@ -5,5 +5,14 @@
 // EXAMPLE:
 // n=5 //returns [5,4,3,2,1]
 export const reverseSeq = (n: number) => {
-  return false;
+  //initialize empty array for the countdown
+  const countDownArray: number[] = [];
+
+  //if the number is greater than 0, it pushes the number to the array and decrements 
+  for (let index = n; index > 0; index--) {
+    countDownArray.push(index);
+  }
+
+  //return the FINAL COUNTDOWN!!!!
+  return countDownArray;
 };
